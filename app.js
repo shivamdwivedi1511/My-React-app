70c694717196fc9a1d4da16c987d4df25bd6a607
shivamdwivedi1511 <shivamdwivedi1511@gmail.com>
2023-04-13 21:36:27 +0530

    code using JSX

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//React without JSX
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const ele = React.createElement('h1', {style:{color:'red'}}, 'Rendered using React');
-root.render(ele);
+
+
+//React with JSX
+
+const Heading = ({text})=> (<h1>{text}</h1>)
+
+const MainSection =()=>{
+    return (
+        <React.Fragment>
+            <Heading text='Random Number Generator' />
+        <div className='container'>
+            <h2 style={{color:'darkBlue'}}>Refresh page to get a random number</h2>
+            <h3 style={{color:'purple'}}>{Math.floor(Math.random()*100)}</h3>
+        </div>
+        </React.Fragment>
+    )
+}
+root.render(<MainSection />);
