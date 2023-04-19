fd32864141cd56bc17c2c0c9d57a75b3a472efab
shivamdwivedi1511 <shivamdwivedi1511@gmail.com>
2023-04-19 22:22:37 +0530

    added hooks to our app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import { DATA_LIST } from "../config";
 import Card from "./Card";
 
 const Body =()=>{
+    const getData =async()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'KP3ctoGzvMmshGOLIYxNNmkegM4Ap1Vl22cjsnfiGfDP7immud',
+                'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+        };
+        
+        let res = await  fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+        // let data = await JSON.parse(res);
+        console.log('data>>>>', res);
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
     return (
         <div className="body">
            {DATA_LIST.map(item=><Card data={item.data} />)}
